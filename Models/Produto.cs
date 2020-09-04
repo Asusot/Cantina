12b5a73fd109b69b33a30e5c3ab6a86cf8423d7c
rafael conrado <rafael070893@hotmail.com>
2020-09-03 23:08:14 -0300

    Lista de produtos e botao cadastrar

---
+++ Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace canteena.Models
+{
+   public class Produto
+    {
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+        public string ImgUrl { get; set; }
+
+        public Produto()
+        {
+        }
+
+    }
+}
